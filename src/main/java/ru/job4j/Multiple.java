6bc038068301d169dc6af260f290aba3c23db693
Andrey <2884868@gmail.com>
2022-04-10 21:06:17 +0200

    6. Checkstyle. [#513358]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class Multiple {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("1 * 9 = 9");
         System.out.println("2 * 9 = 18");
         System.out.println("3 * 9 = 27");
