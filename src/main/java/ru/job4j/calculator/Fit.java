a206a9f7adfb07c7cea982f7e3efb63a26b0b52b
Andrey <2884868@gmail.com>
2022-04-13 21:30:57 +0200

    1.1. Что такое import 2 [#513358]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,21 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(int height) {
+        return (height - 100) * 1.15;
+    }
+
+    public static double womanWeight(int height) {
+        return (height - 100) * 1.1;
+    }
+
+    public static void main(String[] args) {
+        double man = Fit.manWeight(187);
+        System.out.println("Man 187 is " + man);
+
+        double woman = Fit.womanWeight(170);
+        System.out.println("Woman 170 is " + woman);
+    }
+
+}
